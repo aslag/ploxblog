8f7641c6e517037a0ae8b54bf90b186c5437f63a
aslag <aslag@tehlulz.com>
2014-09-15 17:35:37 -0600

    added support for common HTTP methods with dummy response generation

--- src/ploxblog/handlers/api.clj
+++ src/ploxblog/handlers/api.clj
@@ -5,17 +5,47 @@
             [taoensso.timbre :as timbre]))
 (timbre/refer-timbre)
 
+(defn by-id [id]
+  {:id "some_id"})
+
 (def api-routes
   (routes
     (context "/api" []
       (context "/post" []
+        ; get all
+        (GET "/" []
+          (info "GET /api/post")
+          (response [{:id "some_id"}, {:id "some_other_id"}]))
+        ; get meta about one by id
+        (HEAD "/:id" [id]
+          (info "HEAD" (str "/api/post/" id))
+          (dissoc (response (by-id id)) :body))
+        ; get one by id
         (GET "/:id" [id]
-          (info (str "GET /api/post/" id))
-          (response {:message "foof"}))
+          (info "GET" (str "/api/post/" id))
+          (response (by-id id)))
+        ; create new, returns new w/ id
         (POST "/" {body :body}
-          (info (str "POST /api/post/ with body" body))
-          (response {:message "goof"})
-              ))
+          (info "POST /api/post" body)
+          (response {:id "some_id"}))
+        ; delete one by id, returns deleted
+        (DELETE "/:id" [id]
+          (info "DELETE" (str "/api/post/" id))
+          (response {:id "some_id"}))
+        ; entire update of one by id, returns updated
+        (PUT "/:id" [id :as {body :body}]
+          (info "PUT" (str "/api/post/" id) body)
+          (response {:id "some_id"}))
+        ; partial update of one by id, returns updated
+        (PATCH "/:id" [id :as {body :body}]
+          (info "PATCH" (str "/api/post/" id) body)
+          (response {:id "some_id"}))
+        ; available methods on resource
+        (OPTIONS "/:id" [id]
+          (info "OPTIONS" (str "/api/post/" id))
+          ; TODO: fix!
+          (header {} "Allow" "HEAD,<others particular to resource>"))
+      )
       ; removed b/c I hope to handle these more generally
       ;
       ; (route/not-found (response {:message "not found"}))
